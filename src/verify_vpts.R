3f2083f579f10cd8d8aa042124dadcd2dbd9eb4e
Peter Desmet <peter.desmet@inbo.be>
2023-09-06 16:33:38 +0200

    Split functionality in 3 parts and save end result as "compare"

--- src/verify_vpts.R
+++ src/verify_vpts.R
@@ -1,21 +1,26 @@
 library(tidyverse)
 library(data.table)
 
-# reduce aws ls results to radar + isodate identity chr
-found <-
+# Read aws s3 ls results and reduce to identity (radaryyyymmdd) and present = TRUE
+vpts_daily <-
   data.table::fread("data/aws_ls.txt", select = 4)$V4 %>%
   basename() %>%
   stringr::str_sub(end = -5L) %>%
   stringr::str_remove("_vpts_") %>% 
   tibble(identity = ., present = TRUE)
 
-# split and recombine, then match on radar + isodate identity chr
-readr::read_csv("data/coverage.csv", col_select = "directory", show_col_types = FALSE) %>% 
+# Read coverage.csv and split directory
+hdf5 <-
+  readr::read_csv("data/coverage.csv", col_select = "directory", show_col_types = FALSE) %>% 
   tidyr::separate_wider_delim(
     directory,
     delim = "/",
     names = c("source", "format", "radar", "year", "month", "day")
   ) %>% 
-  mutate(identity = paste0(radar,year,month,day)) %>% 
-  left_join(found, by = join_by(identity)) %>% 
+  mutate(identity = paste0(radar, year, month, day))
+
+# Compare files
+compare <-
+  hdf5 %>%
+  left_join(vpts_daily, by = join_by(identity)) %>% 
   tidyr::replace_na(list(present = FALSE))
